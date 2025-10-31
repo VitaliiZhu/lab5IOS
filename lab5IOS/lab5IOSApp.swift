274e4d2a4bbfcd23b1b4b08263ccdac361a3f797
VitaliiZhu <23zhuk.v@nltu.lviv.ua>
2025-10-31 09:01:18 +0200

    Initial Commit

---
+++ lab5IOS/lab5IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  lab5IOSApp.swift
+//  lab5IOS
+//
+//  Created by ІПЗ-31/1 on 31.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct lab5IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
