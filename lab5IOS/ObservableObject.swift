a14f5fb9b1114a5756cefd3087b277bbbee16487
VitaliiZhu <23zhuk.v@nltu.lviv.ua>
2025-10-31 10:57:42 +0200

    finish

---
+++ lab5IOS/ObservableObject.swift
@@ -0,0 +1,8 @@
+//
+//  ObservableObject.swift
+//  lab5IOS
+//
+//  Created by ІПЗ-31/1 on 31.10.2025.
+//
+
+import Foundation
---
+++ lab5IOS/getInfo.swift
@@ -0,0 +1,8 @@
+//
+//  getInfo.swift
+//  lab5IOS
+//
+//  Created by ІПЗ-31/1 on 31.10.2025.
+//
+
+import Foundation
